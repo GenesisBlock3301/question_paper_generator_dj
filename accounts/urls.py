3c3779c146afa869583e5712b1914fc95c9e3fcb
TrillionBits <devteam.trillionbits@gmail.com>
2021-12-07 22:19:58 +0600

    authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path("", views.test,name="test"),
     path("login/", views.LoginView.as_view(),name="login"),
     path("register/", views.RegistrationView.as_view(),name="register"),
-    path("aregister/", views.AdminRegisterView.as_view(),name="aregister"),
+    path("aregister/", views.AdminRegistrationView.as_view(),name="aregister"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,78 @@
-from django.shortcuts import render,HttpResponse
-from . import models
+from django.shortcuts import redirect, render, HttpResponse
+from . models import User
 from django.views import View
-
-def test(request):
-    data = "Nayma Islam"
-    lis = [1,2,3,4,5,]
-    return HttpResponse(f"<div>Hello {data}</div>")
+from django.contrib import messages
+from django.contrib.auth import login as user_login, authenticate
 
 
 class LoginView(View):
-    def get(self,request):
-        return render(request,"login.html")
+    def get(self, request):
+        return render(request, "login.html")
+    
+    def post(self,request):
+        email = request.POST.get('email', None)
+        password = request.POST.get('password', None)
+        user = authenticate(email=email, password=password)
+        print(user)
+        if user is not None:
+            user_login(request,user)
+            return redirect('questions')
+        return render(request, 'login.html')
+
 
 class RegistrationView(View):
-    def get(self,request):
-        return render(request,'register.html')
+    def get(self, request):
+        return render(request, 'register.html')
 
-class AdminRegisterView(View):
-    def get(self,request):
-        return render(request,'admin_reg.html')
+    def post(self, request):
+        email = request.POST.get("email", None)
+        password = request.POST.get("password", None)
+        cpassword = request.POST.get("cpassword", None)
+        print(email, password, cpassword)
+        if password == cpassword:
+            if len(password) < 8:
+                messages.error("Password length must be greater than 8 digit")
+            else:
+                user = User.objects.filter(email=email).exists()
+                if not user:
+                    user = User.objects.create_user(
+                        email=email, password=password)
+                    user.is_teacher = True
+                    user.is_active = False
+                    user.save()
+                    messages.success(request, "Successfully created user!")
+                    return redirect("login")
+                else:
+                    messages.error(request, "User already exists")
+        else:
+            messages.error("Password not match!")
+        return render(request, 'register.html')
+
+
+class AdminRegistrationView(View):
+    def get(self, request):
+        return render(request, 'admin_reg.html')
+
+    def post(self, request):
+        email = request.POST.get("email", None)
+        password = request.POST.get("password", None)
+        cpassword = request.POST.get("cpassword", None)
+
+        if password == cpassword:
+            if len(password) < 8:
+                messages.error("Password length must be greater than 8 digit")
+            else:
+                user = User.objects.filter(email=email)
+                if not user.exists():
+                    user = User.objects.create_user(
+                        email=email, password=password)
+                    user.is_staff = True
+                    user.is_superuser = True
+                    user.is_active = False
+                    user.save()
+                    messages.success(request, "Successfully created user!")
+                    return redirect("login")
+                else:
+                    messages.error(request, "User already exists")
+        else:
+            messages.error("Password not match!")
